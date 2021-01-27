8572e6524c332cdfb3561953b454348f2bf91061
Melody Daniel <melodyogonna@gmail.com>
2021-01-27 15:46:46 +0100

    Appointment model

--- src/core/models.py
+++ src/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+
+USER = get_user_model()
 
 # Create your models here.
+class Appointments(models.Model):
+    """Table to hold doctor's appointments"""
+
+    patient = models.ForeignKey(USER, related_name="patient", on_delete=models.CASCADE)
+    doctor = models.ForeignKey(USER, related_name="doctor", on_delete=models.CASCADE)
+    appointment_date = models.DateTimeField()
